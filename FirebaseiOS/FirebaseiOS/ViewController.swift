e96eba934ad611ce1e68c066a98510e2f05ff8e6
AlvarDev <deivid07.21@gmail.com>
2017-03-12 23:59:56 -0300

    Android: save data on firebase (Realtime) iOS: set firebase, save data on firebase (Realtime)

--- FirebaseiOS/FirebaseiOS/ViewController.swift
+++ FirebaseiOS/FirebaseiOS/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+  
+    
+    @IBOutlet weak var typeMessage: UITextField!
+    @IBOutlet weak var messages: UITextView!
+    
+    
+    let rootRef = FIRDatabase.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let message = rootRef.child("message")
+        
+        message.observe(FIRDataEventType.value, with: { (snapshot) in
+            let chat = snapshot.value as? String
+            self.messages.text = chat
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sendMessage(_ sender: Any) {
+        if (typeMessage.text?.characters.count)! > 0{
+            let message = (messages.text.characters.count == 0 ? "" : messages.text + "\n") +
+                "iOS: " + typeMessage.text!
+            typeMessage.text = ""
+            rootRef.child("message").setValue(message)
+        }else{
+            print("Message is empty")
+        }
     }
+    
+}
 
-
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
 }
 
