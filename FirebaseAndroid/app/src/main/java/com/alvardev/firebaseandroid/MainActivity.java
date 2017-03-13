df9b125ff002719bff491aaf2f79cde4c407b350
AlvarDev <deivid07.21@gmail.com>
2017-03-12 21:50:34 -0300

    Add Firebase Real time database

--- FirebaseAndroid/app/src/main/java/com/alvardev/firebaseandroid/MainActivity.java
+++ FirebaseAndroid/app/src/main/java/com/alvardev/firebaseandroid/MainActivity.java
@@ -2,12 +2,52 @@ package com.alvardev.firebaseandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @BindView(R.id.tv_message) protected TextView tvMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        //myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                tvMessage.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                tvMessage.setText(":(");
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
 }
